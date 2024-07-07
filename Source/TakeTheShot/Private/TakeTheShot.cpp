5c8e3b44290169cf39343d5febe394d4f8ffd681
陈华 <44075904+Devil-Beichen@users.noreply.github.com>
2024-07-07 16:05:20 +0800

    项目初始化

---
+++ Source/TakeTheShot/Private/TakeTheShot.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TakeTheShot.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TakeTheShot, "TakeTheShot" );
