dd421c44ff02d53a7c82daa871da75fdfe62f32d
陈华 <44075904+Devil-Beichen@users.noreply.github.com>
2024-07-12 00:30:12 +0800

    全局LuaDebug文件添加，方便Lua的Debug，创建了一个游戏实例

---
+++ Content/Script/Blueprints/GI_TakeTheShot.lua
@@ -0,0 +1,14 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+require("UnLuaDebug")
+
+---@type GI_TakeTheShot_C
+local M = UnLua.Class()
+
+return M
---
+++ Content/Script/UnLuaDebug.lua
@@ -0,0 +1,5 @@
+﻿local Path = UE.UKismetSystemLibrary:GetProjectDirectory()
+Path = ";" .. Path .. "Emmy/?.dll"
+package.cpath = package.cpath .. Path
+local dbg = require('emmy_core')
+dbg.tcpConnect('localhost', 9966)
