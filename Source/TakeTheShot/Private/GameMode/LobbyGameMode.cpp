09b9dddc207e5ed7b493a12fed6b6664c8a05b7f
陈华 <44075904+Devil-Beichen@users.noreply.github.com>
2024-07-28 18:33:53 +0800

    大厅地图初步制作

---
+++ Source/TakeTheShot/Private/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameMode/LobbyGameMode.h"
+
+#include "GameFramework/GameStateBase.h"
+
+ALobbyGameMode::ALobbyGameMode()
+{
+	// 游戏是否使用在后台加载的SeamlessTravel()执行地图旅行，并且不会断开客户端连接
+	bUseSeamlessTravel = true;
+}
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	// 获取当前游戏状态中玩家的数量
+	// 通过GameState的Get方法获取全局游戏状态实例，然后调用其PlayerArray属性的Num方法来计算玩家数量
+	int32 NumberOfPlayer = GameState.Get()->PlayerArray.Num();
+	if (NumberOfPlayer == 2)
+	{
+		if (UWorld* World = GetWorld())
+		{
+			World->ServerTravel(FString("/Game/Maps/BlasterMap?listen"));
+		}
+	}
+}
---
+++ Source/TakeTheShot/Public/GameMode/LobbyGameMode.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LobbyGameMode.generated.h"
+
+/**	大厅游戏模式基类
+ * 
+ */
+UCLASS()
+class TAKETHESHOT_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+	ALobbyGameMode();
+	
+public:
+	/**	在玩家登录后调用的函数。
+	* 该函数被设计为在玩家控制器成功登录后被调用。它提供了一个钩子，
+	* 允许子类在登录过程完成后执行自定义逻辑。例如，可以使用这个函数来初始化
+	* 玩家的状态，加载玩家的存档，或者设置玩家的初始位置和朝向。
+	* @param NewPlayer 指向新登录玩家控制器的指针。这个参数允许函数与特定的玩家交互，比如设置玩家的视角或者向玩家发送消息。
+	*/
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+	// 在BP中实现玩家登录
+	UFUNCTION(BlueprintImplementableEvent)
+	void PostLogin_BP(APlayerController* NewPlayer);
+};
