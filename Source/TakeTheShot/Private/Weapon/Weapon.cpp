309330167650d052939cbc9920c731e1a0a9dd2e
陈华 <44075904+Devil-Beichen@users.noreply.github.com>
2024-07-31 00:58:51 +0800

    武器基类添加，以及基础属性设置

---
+++ Source/TakeTheShot/Private/Weapon/Weapon.cpp
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/Weapon.h"
+#include "Components/SphereComponent.h"
+
+
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	// 创建默认子对象USkeletalMeshComponent，用于表示武器的网格组件
+	// 使用文本"WeaponMesh"作为名称，以便在编辑器中识别
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+
+	// 将WeaponMesh设置为根组件的附件
+	// 这样做可以使WeaponMesh随着根组件一起移动和旋转
+	WeaponMesh->SetupAttachment(RootComponent);
+
+	// 设置根组件为WeaponMesh
+	// 这一步是将WeaponMesh作为刚体的主要组件，用于物理交互
+	SetRootComponent(WeaponMesh);
+
+	// 设置WeaponMesh的碰撞响应为阻挡所有通道
+	// 使得WeaponMesh能够阻止其他物体穿过它
+	WeaponMesh->SetCollisionResponseToChannels(ECollisionResponse::ECR_Block);
+
+	// 设置WeaponMesh对Pawn通道的碰撞响应为忽略
+	// 这样武器不会阻止角色的移动和交互
+	WeaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	// 禁止WeaponMesh的碰撞检测
+	// 即使其他物体与WeaponMesh相交，也不会产生碰撞效果
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	// 创建默认子对象USphereComponent，用于表示武器的影响区域
+	// 使用文本"AreaSphere"作为名称，以便在编辑器中识别
+	AreaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreaSphere"));
+
+	// 将AreaSphere设置为根组件的附件
+	// 与WeaponMesh类似，AreaSphere也会随着根组件一起移动和旋转
+	AreaSphere->SetupAttachment(RootComponent);
+
+	// 设置AreaSphere对所有通道的碰撞响应为忽略
+	// 这样AreaSphere不会影响其他物体的运动和交互
+	AreaSphere->SetCollisionResponseToChannels(ECollisionResponse::ECR_Ignore);
+
+	// 禁止AreaSphere的碰撞检测
+	// 与WeaponMesh相同，AreaSphere不会产生碰撞效果
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	/*
+	 * 如果当前对象具有权限，则设置AreaSphere的碰撞属性。
+	 * 这里的目的是让AreaSphere对Pawn类型的对象只进行碰撞检测，而不进行碰撞响应，
+	 * 也就是说，Pawn可以穿过AreaSphere，但AreaSphere不会对Pawn产生碰撞效果。
+	 */
+	if (HasAuthority())
+	{
+		// 启用碰撞检测和物理碰撞
+		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		// 对于Pawn类型的对象，设置碰撞响应为重叠，即不产生碰撞效果
+		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	}
+
+}
+
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/TakeTheShot/Public/Weapon/Weapon.h
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState :uint8
+{
+	//  装备的初始状态。指示装备是新获得的，尚未被装备。
+	EWS_Initial UMETA(DisplayName = "Initial State"),
+	//  装备已装备。指示装备已装备到角色身上。
+	EWS_Equipped UMETA(DisplayName = "Equipped"),
+	//  装备已丢弃。指示装备已被丢弃。
+	EWS_Dropped UMETA(DisplayName = "Dropped"),
+
+	// 装备穿戴状态的最大数量。这是一个占位符值，用来指示枚举的结束。
+	EWS_MAX UMETA(DisplayName = "DefaultMAX")
+};
+
+/**	武器基类
+ * 
+ */
+UCLASS()
+class TAKETHESHOT_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	// 武器模型组件，用于可视化展示武器
+	// 类型为USkeletalMeshComponent，允许在任何地方可见
+	UPROPERTY(VisibleAnywhere, Category="Weapon Properties")
+	USkeletalMeshComponent* WeaponMesh = nullptr;
+
+	// 武器影响区域组件，以球形定义影响范围
+	// 类型为USphereComponent，允许在任何地方可见
+	UPROPERTY(VisibleAnywhere, Category="Weapon Properties")
+	class USphereComponent* AreaSphere = nullptr;
+
+	// 武器状态变量，用于表示武器的当前状态
+	// 取值自EWeaponState枚举，初始状态为EWS_Initial
+	UPROPERTY(VisibleAnywhere)
+	EWeaponState WeaponState = EWeaponState::EWS_Initial;
+
+public:
+};
