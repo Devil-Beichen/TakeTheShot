22fa1ad89e49b83bd29dc6988d86b3da7b08cf1f
陈华 <2394439184@qq.com>
2024-08-29 19:02:27 +0800

    重生初步制作

--- Source/TakeTheShot/Private/GameMode/BlasterGameMode.cpp
+++ Source/TakeTheShot/Private/GameMode/BlasterGameMode.cpp
@@ -4,11 +4,38 @@
 #include "GameMode/BlasterGameMode.h"
 
 #include "Character/BlasterCharacter.h"
+#include "GameFramework/PlayerStart.h"
+#include "Kismet/GameplayStatics.h"
 
 void ABlasterGameMode::PlayerEliminated(ABlasterCharacter* EliminatedCharacter, ABlasterPlayerController* VictimController, ABlasterPlayerController* AttackerController)
 {
 	if (EliminatedCharacter)
 	{
 		EliminatedCharacter->Elim();
+	}
+}
+
+void ABlasterGameMode::RequestRespawn(ACharacter* EliminatedCharacter, AController* EliminatedController)
+{
+	// 如果被消除的角色对象存在
+	if (EliminatedCharacter)
+	{
+	    // 重置角色对象，准备重新开始
+	    EliminatedCharacter->Reset();
+	    // 销毁角色对象，释放资源
+	    EliminatedCharacter->Destroy();
+	}
+	
+	// 如果被消除的控制器对象存在
+	if (EliminatedController)
+	{
+	    // 找到所有玩家起点对象
+	    TArray<AActor*> PlayerStarts;
+	    UGameplayStatics::GetAllActorsOfClass(this, APlayerStart::StaticClass(), PlayerStarts);
+	
+	    // 随机选择一个玩家起点
+	    const int32 Selection = FMath::RandRange(0, PlayerStarts.Num() - 1);
+	    // 让被消除的玩家在新的起点重新开始游戏
+	    RestartPlayerAtPlayerStart(EliminatedController, PlayerStarts[Selection]);
 	}
 }
--- Source/TakeTheShot/Public/GameMode/BlasterGameMode.h
+++ Source/TakeTheShot/Public/GameMode/BlasterGameMode.h
@@ -26,8 +26,16 @@ public:
 	 * @param AttackerController 攻击者的控制器对象，类型为ABlasterPlayerController*。
 	 */
 	virtual void PlayerEliminated(
-	    ABlasterCharacter* EliminatedCharacter,
-	    ABlasterPlayerController* VictimController,
-	    ABlasterPlayerController* AttackerController
+		ABlasterCharacter* EliminatedCharacter,
+		ABlasterPlayerController* VictimController,
+		ABlasterPlayerController* AttackerController
 	);
+
+	/** 请求复活角色
+	 * 当一个角色被消除时，调用此函数来请求复活。
+	 * 
+	 * @param EliminatedCharacter 被消除的角色对象指针。
+	 * @param EliminatedController 被消除角色的控制器对象指针。
+	 */
+	virtual void RequestRespawn(ACharacter* EliminatedCharacter, AController* EliminatedController);
 };
