93313bc5994bdeece60c34e17e877784f083da09
陈华 <2394439184@qq.com>
2025-01-28 11:34:58 +0800

    团队游戏模式创建。并编写基础逻辑

---
+++ Source/TakeTheShot/Private/GameMode/TeamsGameMode.cpp
@@ -0,0 +1,98 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameMode/TeamsGameMode.h"
+
+#include "GameState/BlasterGameState.h"
+#include "Kismet/GameplayStatics.h"
+#include "PlayerState/BlasterPlayerState.h"
+
+// 玩家登录时
+void ATeamsGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	// 获取游戏状态
+	if (ABlasterGameState* BGameState = Cast<ABlasterGameState>(UGameplayStatics::GetGameState(this)))
+	{
+		// 将玩家状态转换为Blaster玩家状态，并检查玩家是否尚未分配团队
+		if (ABlasterPlayerState* BPState = NewPlayer->GetPlayerState<ABlasterPlayerState>();
+			BPState && BPState->GetTeam() == ETeam::ET_NoTeam)
+		{
+			// 如果蓝队的人数大于等于红队的人数，则将玩家分配到红队，否则分配到蓝队
+			if (BGameState->BlueTeam.Num() >= BGameState->RedTeam.Num())
+			{
+				BPState->SetTeam(ETeam::ET_RedTeam);
+				BGameState->RedTeam.AddUnique(BPState);
+			}
+			else
+			{
+				BPState->SetTeam(ETeam::ET_BlueTeam);
+				BGameState->BlueTeam.AddUnique(BPState);
+			}
+		}
+	}
+}
+
+// 玩家退出时
+void ATeamsGameMode::Logout(AController* Exiting)
+{
+	Super::Logout(Exiting);
+	// 获取游戏状态
+	if (ABlasterGameState* BGameState = Cast<ABlasterGameState>(UGameplayStatics::GetGameState(this)))
+	{
+		// 获取退出玩家的状态
+		if (ABlasterPlayerState* BPState = Exiting->GetPlayerState<ABlasterPlayerState>())
+		{
+			// 检查并移除玩家状态
+			if (BGameState->RedTeam.Find(BPState))
+			{
+				BGameState->RedTeam.Remove(BPState);
+			}
+			else if (BGameState->BlueTeam.Find(BPState))
+			{
+				BGameState->BlueTeam.Remove(BPState);
+			}
+		}
+	}
+}
+
+// 比赛开始时
+void ATeamsGameMode::HandleMatchHasStarted()
+{
+	Super::HandleMatchHasStarted();
+
+	TeamGroup();
+}
+
+/**
+ * 该函数用于在游戏模式中对玩家进行团队分组。
+ * 它根据当前游戏状态中的玩家数量，将未分配团队的玩家分配到人数较少的团队中。
+ */
+void ATeamsGameMode::TeamGroup()
+{
+	// 获取游戏状态
+	if (ABlasterGameState* BGameState = Cast<ABlasterGameState>(UGameplayStatics::GetGameState(this)))
+	{
+		// 遍历游戏状态中的所有玩家
+		for (auto& PState : BGameState->PlayerArray)
+		{
+			// 将玩家状态转换为Blaster玩家状态，并检查玩家是否尚未分配团队
+			if (ABlasterPlayerState* BPState = Cast<ABlasterPlayerState>(PState.Get());
+				BPState && BPState->GetTeam() == ETeam::ET_NoTeam)
+			{
+				// 如果蓝队的人数大于等于红队的人数，则将玩家分配到红队，否则分配到蓝队
+				if (BGameState->BlueTeam.Num() >= BGameState->RedTeam.Num())
+				{
+					BPState->SetTeam(ETeam::ET_RedTeam);
+					BGameState->RedTeam.AddUnique(BPState);
+				}
+				else
+				{
+					BPState->SetTeam(ETeam::ET_BlueTeam);
+					BGameState->BlueTeam.AddUnique(BPState);
+				}
+			}
+		}
+	}
+}
---
+++ Source/TakeTheShot/Public/GameMode/TeamsGameMode.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BlasterGameMode.h"
+#include "TeamsGameMode.generated.h"
+
+/**
+ * 团队游戏模式
+ */
+UCLASS()
+class TAKETHESHOT_API ATeamsGameMode : public ABlasterGameMode
+{
+	GENERATED_BODY()
+
+public:
+	// 玩家加入时
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+	// 玩家退出时
+	virtual void Logout(AController* Exiting) override;
+
+protected:
+	// 匹配开始时
+	virtual void HandleMatchHasStarted() override;
+
+	// 团队分组
+	void TeamGroup();
+};
